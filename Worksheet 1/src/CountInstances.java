9c7fa95cda94c9e6329e8f62e6e2ae7dd6c6c8ea
Rafnazahg <devm.class@gmail.comdevm.class@gmail.comdem.class@gmail.com>
2018-07-19 10:45:35 +0300

    Initial Commit

---
+++ Worksheet 1/src/CountInstances.java
@@ -0,0 +1,32 @@
+public class CountInstances {
+
+    // class variable set to 0
+    private static int numInstances = 0;
+
+    // class getter method to get the numInstaces value
+    protected static int getNumInstaces() {
+        return numInstances;
+    }
+
+    // class method to add the numInstance values
+    private static void addInstace() {
+        numInstances++;
+    }
+
+    CountInstances() {
+        CountInstances.addInstace();
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting with " +
+        CountInstances.getNumInstaces() + " instances");
+
+        for (int i = 0; i < 10; i++) {
+            new CountInstances();
+
+            System.out.println("Created " +
+            CountInstances.getNumInstaces() + " instances");
+        }
+    }
+}
