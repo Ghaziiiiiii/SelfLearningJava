cfca30339de109cbb8ac5d08bf6f556bb87f15d6
Ghaziiiiiii <devm.class@gmail.comdevm.class@gmail.comdem.class@gmail.com>
2018-07-21 21:34:02 +0300

    Second Committ Additional objects (Salary object) method call: mailCheck()

--- Worksheet 1/src/AbstractDemo.java
+++ Worksheet 1/src/AbstractDemo.java
@@ -3,16 +3,15 @@ File name : AbstractDemo.java
  */
 public class AbstractDemo {
     public static void main(String[] args) {
-        /*
-        Following is not allowed & would raise error.
-        'Employee' is abstract; can not be instantiated.
 
-        'class' or 'interface' expected
-         */
+        Salary s = new Salary("Miagi Yushimito", "Tokyo, Japan", 5, 50000.00);
+
         Employee e = new Employee("George W.", "Houston, TX", 43);
 
-        System.out.println("\n Call mailCheck using Employee reference--");
+        System.out.println("Call mailChec using Salary reference --");
+        s.mailCheck();
 
+        System.out.println("\n Call mailCheck using Employee reference--");
         e.mailCheck();
 
         }
