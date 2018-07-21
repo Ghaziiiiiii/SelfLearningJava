b7727f63fc12066ab06f2876f0b72bdd2a424490
Ghaziiiiiii <devm.class@gmail.comdevm.class@gmail.comdem.class@gmail.com>
2018-07-21 20:45:50 +0300

    Initial Commit

---
+++ Worksheet 1/src/Employee.java
@@ -0,0 +1,46 @@
+/*
+File name : Employee.java
+ */
+public abstract class Employee {
+    // instance variable
+    private String name;
+    private String address;
+    private int number;
+
+    // constructor method invoked to create an object
+    public Employee(String name, String address, int number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public double computePay() {
+        System.out.println("Inside Employee computePay");
+        return 0.0;
+    }
+
+    public void mailCheck() {
+        System.out.println("Mailing a check to " + this.name + " ");
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + address + " " + number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String newAddress) {
+        address = newAddress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
